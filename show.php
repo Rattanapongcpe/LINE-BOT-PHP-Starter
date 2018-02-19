e69d1ca0646f90cc24ba739e89bb8d2b7ffc0f46
Rattanapongcpe <35572641+Rattanapongcpe@users.noreply.github.com>
2018-02-20 02:50:15 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,3 +1,20 @@
 <?php
-echo "hello";
+function Connection(){
+	$server="localhost";
+	$user="root";
+	$pass="hctr77hd";
+	$db="hydro";
+	$connection = mysql_connect($server, $user, $pass);
+  echo "ok";
+// 	if (!$connection) {
+// 		die('MySQL ERROR: ' . mysql_error());
+// 	}
+// 	mysql_select_db($db) or die( 'MySQL ERROR: '. mysql_error() );
+// 	return $connection;
+// }
+// 	$link=Connection();
+// 	$result = mysql_query ("SELECT * FROM sensor_data ORDER BY timeStamp DESC",$link) or die("ไม่สามารถเลือกฐานข้อมูลได้");
+// 	$row = mysql_fetch_array($result);
+	
+// 	echo $row['pH']
 ?>

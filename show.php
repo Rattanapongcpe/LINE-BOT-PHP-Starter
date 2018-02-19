af79855294c83cb2d1b1c0ee12a0977b7ec1f1c0
Rattanapongcpe <35572641+Rattanapongcpe@users.noreply.github.com>
2018-02-19 15:45:08 +0700

    Update show.php

--- show.php
+++ show.php
@@ -20,6 +20,5 @@ function Connection(){
 	$result = mysql_query ("SELECT * FROM sensor_data ORDER BY timeStamp DESC",$link) or die("ไม่สามารถเลือกฐานข้อมูลได้");
 	$row = mysql_fetch_array($result);
 	
-	echo $row["timeStamp"], $row["temperature"], $row["humidity"], $row["light"], $row["pH"], $row["pump"], $row["fog"];
-
+	echo $row["timeStamp"];
 ?>

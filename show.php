c700c54d448cc3d5721ae6cec754d45b5920dadb
Rattanapongcpe <35572641+Rattanapongcpe@users.noreply.github.com>
2018-02-20 02:47:19 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,24 +1,3 @@
 <?php
-function Connection(){
-	$server="localhost";
-	$user="root";
-	$pass="hctr77hd";
-	$db="hydro";
-
-	$connection = mysql_connect($server, $user, $pass);
-
-	if (!$connection) {
-		die('MySQL ERROR: ' . mysql_error());
-	}
-
-	mysql_select_db($db) or die( 'MySQL ERROR: '. mysql_error() );
-
-	return $connection;
-}
-	$link=Connection();
-
-	$result = mysql_query ("SELECT * FROM sensor_data ORDER BY timeStamp DESC",$link) or die("ไม่สามารถเลือกฐานข้อมูลได้");
-	$row = mysql_fetch_array($result);
-	
-	echo $row['pH']
+echo ["hello"];
 ?>
